8395b0cd94c3a3ce6e93cd2d9dcb7cd57d85dbae
randyqlee <44831457+randyqlee@users.noreply.github.com>
2019-08-19 18:05:31 +0800

    menu scene created

--- Assets/Scripts/Logic/TurnManager.cs
+++ Assets/Scripts/Logic/TurnManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 using DG.Tweening;
 
+
 // this class will take care of switching turns and counting down time until the turn expires
 public class TurnManager : MonoBehaviour {
 
