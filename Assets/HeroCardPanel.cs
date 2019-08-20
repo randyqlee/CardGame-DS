a876d5658b5897f686ac765f6f88b7bd100508b5
playstrongph <playstrong.ph@gmail.com>
2019-08-20 12:42:05 +0800

    Hero and Ability Preivews
    Need to Improve Code

---
+++ Assets/HeroCardPanel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroCardPanel : MonoBehaviour {
+
+	public List<GameObject> abilityCards = new List<GameObject>();
+	
+	// Use this for initialization
+	void Awake () {
+			
+			foreach(GameObject go in abilityCards)
+			{
+				Setup();
+			}
+			
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Setup()
+	{
+		int x = 0;
+		int creatureID = GetComponentInParent<IDHolder>().UniqueID;
+		GameObject creature = IDHolder.GetGameObjectWithID(creatureID);
+		CreatureLogic cl = CreatureLogic.CreaturesCreatedThisGame[creatureID];
+		foreach (CreatureEffect ce in cl.creatureEffects)
+		{
+			
+				abilityCards[x].GetComponent<AbilityCardPanel>().cardTitleText.text = ce.abilityDescription;
+				abilityCards[x].GetComponent<AbilityCardPanel>().cardGraphicImage.sprite = ce.abilityPreviewSprite;
+				abilityCards[x].GetComponent<AbilityCardPanel>().manaText.text = ce.creatureEffectCooldown.ToString();
+				abilityCards[x].GetComponent<AbilityCardPanel>().cardTitleText.text = ce.Name;
+				abilityCards[x].GetComponent<AbilityCardPanel>().descriptionText.text = ce.abilityDescription;	
+				x++;
+			
+		}
+
+	}
+}
--- Assets/Scripts/Visual/AbilityCardPanel.cs
+++ Assets/Scripts/Visual/AbilityCardPanel.cs
@@ -5,20 +5,41 @@ using UnityEngine.UI;
 
 public class AbilityCardPanel : MonoBehaviour {
 
+	//public int abilityNo;
+	public Text cardTitleText;
 	public Text descriptionText;
-	public Image cardGraphicImage;
+	public Image cardGraphicImage;	
 	public Text manaText;
-
 	public Image cardGraphicBackground;
 	
 	
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		//Setup();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void Setup()
+	{
+		int creatureID = GetComponentInParent<IDHolder>().UniqueID;
+		GameObject creature = IDHolder.GetGameObjectWithID(creatureID);
+		CreatureLogic cl = CreatureLogic.CreaturesCreatedThisGame[creatureID];
+		foreach (CreatureEffect ce in cl.creatureEffects)
+		{
+			
+				cardTitleText.text = ce.abilityDescription;
+				cardGraphicImage.sprite = ce.abilityPreviewSprite;
+				manaText.text = ce.creatureEffectCooldown.ToString();
+				cardTitleText.text = ce.Name;
+				descriptionText.text = ce.abilityDescription;
+			
+
+			
+		}
+
 	}
 }
