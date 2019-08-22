dd7c2c11ff73fe822f16dc4b7e6ce3657015413e
randyqlee <44831457+randyqlee@users.noreply.github.com>
2019-08-22 11:38:53 +0800

    added sample code to EquipCreatureEffect

--- Assets/Scripts/Logic/CreatureScripts/EquipCreatureEffect.cs
+++ Assets/Scripts/Logic/CreatureScripts/EquipCreatureEffect.cs
@@ -4,11 +4,27 @@ using System.Collections;
 public class EquipCreatureEffect : CreatureEffect
 {
     public EquipCreatureEffect(Player owner, CreatureLogic creature) : base(owner, creature)
-    {}
+    {
+
+        this.owner = owner;
+        this.creature = creature;
+    }
 
     // BATTLECRY
     public override void WhenACreatureIsPlayed()
     {
-        Debug.Log ("Playing Equip Creature: " + owner + "," + creature.ca.name);
+
+        foreach (CreatureLogic cl in owner.AllyList())
+        {
+            if(!cl.isEquip)
+            {
+                cl.Attack += 10;
+                new UpdateAttackCommand(cl.ID, cl.Attack).AddToQueue();
+
+                Debug.Log ("Attack +" + cl.Attack + " to " + cl.ca.name);
+            }
+
+        }
+        
     }
 }
