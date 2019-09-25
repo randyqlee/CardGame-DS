257bc3e92b35c4dd94b0de36ec6e242ab02745eb
randyqlee <44831457+randyqlee@users.noreply.github.com>
2019-09-26 04:30:45 +0800

    added BE:armor

---
+++ Assets/DragonStone/Scripts/BuffEffects/Buffs/Armor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armor : BuffEffect
+{
+  
+	
+    public Armor(CreatureLogic source, CreatureLogic target, int buffCooldown, int specialValue) : base (source, target, buffCooldown, specialValue)
+    {
+        buffIcon = Resources.Load<Sprite>("BuffIcons/Armor");
+        isBuff = true;
+    
+    }
+
+    public override void CauseBuffEffect()
+    {
+        
+        int healthAfter = target.Health + specialValue;
+        new UpdateHealthCommand(target.ID, healthAfter).AddToQueue();
+
+        target.Health += specialValue;    
+    }
+
+    public override void UndoBuffEffect()
+    {
+        int healthAfter = target.Health - specialValue;
+        new UpdateHealthCommand(target.ID, healthAfter).AddToQueue();    
+        
+        target.Health -= specialValue;        
+    }
+
+
+}
--- Assets/DragonStone/Scripts/Logic/BuffEffect.cs
+++ Assets/DragonStone/Scripts/Logic/BuffEffect.cs
@@ -17,6 +17,8 @@ public class BuffEffect
     public bool isBuff;
     public bool isDebuff;
 
+    public int specialValue;
+
     public Sprite buffIcon;
 
     //DS
@@ -28,6 +30,22 @@ public class BuffEffect
         this.source = source;
         this.target = target;
         this.buffCooldown = buffCooldown;
+
+    //DS
+    //Added buffID for Logic and Visual link
+        this.buffID = IDFactory.GetUniqueID();
+
+        Name = this.GetType().Name.ToString();
+        
+
+    }
+
+    public BuffEffect(CreatureLogic source, CreatureLogic target, int buffCooldown, int specialValue)
+    {
+        this.source = source;
+        this.target = target;
+        this.buffCooldown = buffCooldown;
+        this.specialValue = specialValue;
 
     //DS
     //Added buffID for Logic and Visual link
