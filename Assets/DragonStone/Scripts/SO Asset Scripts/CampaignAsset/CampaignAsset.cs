8712a671c918cdbbd6efd6c56399612563c5a15e
randyqlee <44831457+randyqlee@users.noreply.github.com>
2019-12-14 14:10:33 +0800

    added campaign SO

---
+++ Assets/DragonStone/Scripts/SO Asset Scripts/CampaignAsset/CampaignAsset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum HeroSelect
+{
+	Pick,
+	Fixed,
+	Random
+}
+
+public class CampaignAsset : ScriptableObject {
+
+	[Header("Heroes")]
+
+	public HeroSelect AlliesSelect = HeroSelect.Fixed;
+	public List<CardAsset> fixedAllies;
+
+	public List<CardAsset> fixedEnemies;
+
+	[Header("Rewards")]
+	public int gold;
+	public int dust;
+	public List<CardAsset> cards;
+}
---
+++ Assets/DragonStone/Scripts/SO Asset Scripts/CampaignAsset/Editor/CampaignUnityIntegration.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+static class CampaignUnityIntegration {
+
+	[MenuItem("Assets/Create/CampaignAsset")]
+	public static void CreateYourScriptableObject() {
+		ScriptableObjectUtility2.CreateAsset<CampaignAsset>();
+	}
+
+}
